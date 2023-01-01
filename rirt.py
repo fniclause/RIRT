7d90469edf8a3343f3ea2121d0b9597eda523939
Florian Niclause <florianniclause@macbook-air-de-florian.home>
2023-01-01 01:40:17 +0100

    complete CSV and award

--- rirt.py
+++ rirt.py
@@ -1,15 +1,22 @@
 import numpy as np
 from numpy import random
+import pandas as pd
+
+df_recompenses = pd.read_csv("./data/recompenses.csv",index_col=0)
+
+l_recompenses = df_recompenses.index.values.tolist()
+
 
 a = random.normal(0,1)
 
-recompenses = ["Cadre velo","Bouquet de fleurs","Livre victoire","Dérailleur arriere","Pantalon","Chemise blanche","Tri fonction"]
 test = []
 if a <-1 :
     print("test random")
 elif -1<a<=0:
     print("pas de recompense")
 else:
-    print("recompense random")
-    b = random.choice(recompenses)
-    print(f"{b} : +{round(a,2)}€")
+    b = random.choice(l_recompenses)
+    print(f"Bravo, tu te rapproches de l'objectif : {b} => +{round(a,2)}€")
+    df_recompenses.loc[b,"déjà acquis"]=round(a,2)
+    
+df_recompenses.to_csv("./data/recompenses.csv")
