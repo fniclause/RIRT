5b41779cd54d81b2969b194943932e02a5f20620
Florian Niclause <florianniclause@macbook-air-de-florian.home>
2023-01-07 10:34:43 +0100

    add subcategory and rank in activities.csv

--- get_activities.py
+++ get_activities.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-df_activities=pd.read_csv("./data/activities.csv")
+df_activities=pd.read_csv("./data/activities.csv",index_col=[0])
 
-print(df_activities)
+print(df_activities.loc[df_activities["rank"]==1].loc[:,"activities"])
 
 print("What activities did you do ?")
 a=input()
 
 
-if a.lower() in df_activities.loc[:,'activities'].tolist():
+if a.lower() in df_activities.loc[:,'activities'].str.lower().tolist():
     print("OK, let's turn the wheel of fortune.")
     #run the script rirt if the activity is a useful one
     import rirt
